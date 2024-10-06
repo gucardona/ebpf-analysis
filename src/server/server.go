951e8a7e64f676c415ca989d6e35229981d8bdff
Gustavo Parcianello <gustavo.parcianello@sap.com>
2024-10-06 19:28:55 -0300

    change server

--- src/server/server.go
+++ src/server/server.go
@@ -62,7 +62,9 @@ func StartServer(serverPort int) error {
 }
 
 func formatAndPrintMetrics(metricsData []string) {
-	fmt.Println(metricsData[0])
+	split := strings.Split(metricsData[0], "@[")
+
+	fmt.Println(split)
 
 	fmt.Printf("%-30s %s\n", "Metric", "Count")
 	fmt.Println(strings.Repeat("-", 40))
