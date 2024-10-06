b94aade9b26684b4c3e314def2da7995724317d9
Gustavo Parcianello <gustavo.parcianello@sap.com>
2024-10-06 17:28:07 -0300

    change server

--- src/main.go
+++ src/main.go
@@ -17,21 +17,12 @@ func main() {
 	flag.IntVar(&serverPort, "port", 8443, "UDP server port")
 	flag.Parse()
 
-	// Channel to signal when the server is ready
-	serverReady := make(chan struct{})
-
 	go func() {
 		if err := server.StartServer(serverPort); err != nil {
 			log.Fatalf("Failed to start server: %s", err)
 		}
-		// Signal that the server is ready
-		close(serverReady)
 	}()
-
-	// Wait for the server to be ready
-	<-serverReady
-	log.Println("Server is ready, starting the client...")
-
+	
 	if err := client.StartClient(serverPort, messageInterval); err != nil {
 		log.Fatalf("Failed to start client: %s", err)
 	}
