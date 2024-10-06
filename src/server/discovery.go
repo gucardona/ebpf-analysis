e1f1e33d138bc4d4e1d0303bb3ad85ce6cd518c4
Gustavo Parcianello <gustavo.parcianello@sap.com>
2024-10-06 18:08:09 -0300

    change server

--- src/server/discovery.go
+++ src/server/discovery.go
@@ -26,23 +26,24 @@ func StartDiscoveryServer() error {
 	fmt.Println("clients: ", clients)
 	buf := make([]byte, 2048)
 
-	n, remoteAddr, err := conn.ReadFromUDP(buf)
-	if err != nil {
-		fmt.Println("Error receiving discovery message:", err)
-	}
+	for {
+		n, remoteAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving discovery message:", err)
+		}
 
-	message := string(buf[:n])
-	fmt.Println(message)
-	if strings.Contains(message, "register") {
-		clients[remoteAddr.String()] = remoteAddr
-		fmt.Printf("New client registered: %s\n", remoteAddr.String())
+		message := string(buf[:n])
+		fmt.Println(message)
+		if strings.Contains(message, "register") {
+			clients[remoteAddr.String()] = remoteAddr
+			fmt.Printf("New client registered: %s\n", remoteAddr.String())
 
-		for _, clientAddr := range clients {
-			_, err := conn.WriteToUDP([]byte(remoteAddr.String()), clientAddr)
-			if err != nil {
-				fmt.Println("Error sending discovery message to client:", err)
+			for _, clientAddr := range clients {
+				_, err := conn.WriteToUDP([]byte(remoteAddr.String()), clientAddr)
+				if err != nil {
+					fmt.Println("Error sending discovery message to client:", err)
+				}
 			}
 		}
 	}
-	return nil
 }
