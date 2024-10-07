7d6308f8ac97a9e8ba887a12a3dd4060ab340322
Gustavo Parcianello <gustavo.parcianello@sap.com>
2024-10-07 10:26:08 -0300

    aaa

--- src/server/server.go
+++ src/server/server.go
@@ -8,11 +8,12 @@ import (
 	"time"
 )
 
-var serverRegisteredClients []int
+var (
+	serverRegisteredClients []int
+	clientMessages          = make(map[string]string)
+)
 
 func StartServer(serverPort int) error {
-	metricsMap := make(map[string]string)
-
 	addr := net.UDPAddr{
 		Port: serverPort,
 		IP:   net.ParseIP("0.0.0.0"),
@@ -34,78 +35,56 @@ func StartServer(serverPort int) error {
 		}
 
 		message := string(buf[:n])
+		clientKey := remoteAddr.String()
+
 		if strings.Contains(message, "new-client-") {
 			port, ok := strings.CutPrefix(message, "new-client-")
 			if !ok {
-				fmt.Println("Prefix not found to cut: ", err)
+				fmt.Println("Prefix not found to cut:", err)
+				continue
 			}
 			portCnv, err := strconv.Atoi(port)
 			if err != nil {
-				fmt.Println("Error converting port: ", err)
+				fmt.Println("Error converting port:", err)
 				continue
 			}
-			serverRegisteredClients = append(serverRegisteredClients, portCnv)
-		} else {
-			if strings.Contains(message, "probes...") {
-				metricsMap[remoteAddr.String()] = message
-				fmt.Println(strings.Repeat("=", 40))
-				fmt.Println()
+			if !ArrayContains(serverRegisteredClients, portCnv) {
+				serverRegisteredClients = append(serverRegisteredClients, portCnv)
+				fmt.Printf("New client registered: %d\n", portCnv)
+			}
+			continue
+		}
 
-				fmt.Print("\033[H\033[2J")
-				fmt.Printf("Last update: %s\n\n", time.Now().Format(time.RFC3339))
-
-				fmt.Println(metricsMap)
-				for _, metricsData := range metricsMap {
-					formatAndPrintMetrics(metricsData)
-				}
-
-				fmt.Println()
-				fmt.Println(strings.Repeat("=", 40))
+		if strings.Contains(message, "@") {
+			if previousMsg, exists := clientMessages[clientKey]; exists && previousMsg == message {
+				continue
 			}
 
-			if !ArrayContains(serverRegisteredClients, serverPort) {
-				serverRegisteredClients = append(serverRegisteredClients, serverPort)
-			}
+			clientMessages[clientKey] = message
+
+			fmt.Println(strings.Repeat("=", 40))
+			fmt.Println()
+
+			fmt.Print("\033[H\033[2J")
+			fmt.Printf("Last update: %s\n\n", time.Now().Format(time.RFC3339))
+
+			fmt.Println(clientMessages)
+
+			fmt.Println()
+			fmt.Println(strings.Repeat("=", 40))
 
 			for _, clientPort := range serverRegisteredClients {
-				if clientPort != serverPort {
-					fmt.Println("Forwarding to client:", clientPort)
+				if clientPort != serverPort && remoteAddr.Port != clientPort {
+					fmt.Printf("Forwarding to client: %d\n", clientPort)
 					_, err := conn.WriteToUDP([]byte(message), &net.UDPAddr{
 						Port: clientPort,
 						IP:   net.ParseIP("127.0.0.1"),
 					})
 					if err != nil {
-						fmt.Printf("Error sending data to client %d: %s", clientPort, err)
+						fmt.Printf("Error sending data to client %d: %s\n", clientPort, err)
 					}
 				}
 			}
-		}
-	}
-}
-
-func formatAndPrintMetrics(metricsData string) {
-	fmt.Printf("%-30s %s\n", "Metric", "Count")
-	fmt.Println(strings.Repeat("-", 40))
-
-	trim := strings.TrimSpace(metricsData)
-
-	lines := strings.Split(trim, "\n")
-
-	if len(lines) > 1 {
-		lines = lines[1:]
-	}
-
-	for _, line := range lines {
-		nameIndex := strings.Index(line, "@[")
-		quantIndex := strings.Index(line, "]: ")
-
-		if nameIndex != -1 && quantIndex != -1 {
-			name := line[nameIndex+2 : quantIndex]
-			quant := line[quantIndex+3:]
-
-			fmt.Printf("%-30s %s\n", name, quant)
-		} else {
-			fmt.Printf("Invalid line format: %s\n", line)
 		}
 	}
 }
